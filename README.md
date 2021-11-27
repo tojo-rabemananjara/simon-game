7291315f5ad15c5e2e30fd95421056f8cca3f587
Tojo Rabemananjara <trabema1@terpmail.umd.edu>
2021-11-27 05:06:21 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simon Game
+## Introduction
+Simple implementation of classic game of Simon made with Javascript, HTML, and CSS. User tries to match the sequence tiles+colors and get as high a score as possible. Features include, sequence matching, tile images, game sounds.
+## Technology
+* HTML
+* CSS
+* JS
+## Launch
+Go to https://tojo-simon-game.surge.sh
---
+++ game.js
@@ -0,0 +1,71 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+
+function nextSequence() {
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 3);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+//Event handling
+$(".btn").on("click", function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+$(document).on("keydown", function() {
+  if (level === 0) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+  }
+});
+
+function playSound(name) {
+  var randomSound = new Audio("sounds/" + name + ".mp3");
+  randomSound.play();
+}
+
+function animatePress(currentColouor) {
+  $("#" + currentColouor).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColouor).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  //check if most recent user clicked pattern matches game's color at that index
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+
+    //check that user finished their sequence
+    if (currentLevel === gamePattern.length - 1) {
+      setTimeout(function() {
+        nextSequence();
+        userClickedPattern = [];
+      }, 1000);
+    }
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+}
